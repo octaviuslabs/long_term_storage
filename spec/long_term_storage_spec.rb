16a25fc8214885faa7c243ad0576e4387c07a3da
Daniel Jurek <daniel@octaviuslabs.com>
2015-05-24 19:10:51 -0700

    Fix interpretation of AWS URL's... there is no library out there for parsing these directly.

--- spec/long_term_storage_spec.rb
+++ spec/long_term_storage_spec.rb
@@ -5,6 +5,8 @@ describe LongTermStorage do
   let(:correctly_initialized) { LongTermStorage::LongTermStorage.new bucket_name: 'vesper-lts-development' } # 'bucket_name' }
   let(:incorrectly_initialized) { LongTermStorage::LongTermStorage.new }
   let(:test_url) { 'https://s3.amazonaws.com/vesper-lts-development/wawa' }
+  let(:test_url_bucket_dns) { 'https://vesper-lts-development.s3.amazonaws.com/wawa' }
+  let(:public_url_to_object) { LongTermStorage::LongTermStorage.send(:public, :url_to_object) }
 
   it "should respond to store" do
     expect(correctly_initialized).to respond_to(:store)
@@ -28,5 +30,17 @@ describe LongTermStorage do
 
   it "should retrieve given a bucket and key" do
     expect(correctly_initialized.retrieve bucket_name: 'bucket', key: 'key').to eq('')
+  end
+
+  it "should parse bucket/key from dns" do
+    public_url_to_object
+    expect(correctly_initialized.url_to_object test_url_bucket_dns)
+      .to eq(['vesper-lts-development','wawa'])
+  end
+
+  it "should parse bucket/key from path" do
+    public_url_to_object
+    expect(correctly_initialized.url_to_object test_url)
+      .to eq(['vesper-lts-development','wawa'])
   end
 end
